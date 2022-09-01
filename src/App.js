9c3247688c042193fd353e9c810b5214ae1b7abf
nana <comfortappiah.ca@gmail.com>
2022-09-01 22:14:05 +0000

    api calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import ClassApi from "./componets/ClassApi";
+import AxiosAPI from "./componets/AxiosApi";
+import FunctionalAxios from "./componets/FunctionalAxios"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <ClassApi /> */}
+			<FunctionalAxios />
+		</div>
+	);
 }
 
 export default App;
